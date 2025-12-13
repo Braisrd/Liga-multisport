18b675dd5e0f3fcf34f08429ae37f2c2c93d7271
Liga Multisport Admin <admin@ligamultisport.com>
2025-12-13 11:56:35 +0100

    feat: Implement basic PWA support (Manifest + Simple SW)

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'paso-ecuador-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './logo.png',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            // Return cached response if found
+            if (response) {
+                return response;
+            }
+            // Otherwise fetch from network
+            return fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        console.log('[ServiceWorker] Removing old cache', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
